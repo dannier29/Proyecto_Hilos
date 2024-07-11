06a158a823a93fad1bc091e1f0e65b9414a58966
dannier29 <danni.marin@outlook.com>
2024-07-11 17:19:23 -0600

    Ultimos cambios

--- Proyecto1_Hilos/Proyecto Hilos.Designer.cs
+++ Proyecto1_Hilos/Proyecto Hilos.Designer.cs
@@ -36,17 +36,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.TXT_Prueba = new System.Windows.Forms.TextBox();
+            this.TXT_Hilos = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.btn_Salir = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.TXT_Numeros = new System.Windows.Forms.TextBox();
             this.TXT_Tipo = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNumeros)).BeginInit();
@@ -60,7 +59,7 @@
             this.dataGridViewNumeros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewNumeros.Location = new System.Drawing.Point(12, 137);
             this.dataGridViewNumeros.Name = "dataGridViewNumeros";
-            this.dataGridViewNumeros.Size = new System.Drawing.Size(164, 295);
+            this.dataGridViewNumeros.Size = new System.Drawing.Size(135, 295);
             this.dataGridViewNumeros.TabIndex = 0;
             // 
             // dataGridViewFactoriales
@@ -68,7 +67,7 @@
             this.dataGridViewFactoriales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewFactoriales.Location = new System.Drawing.Point(224, 137);
             this.dataGridViewFactoriales.Name = "dataGridViewFactoriales";
-            this.dataGridViewFactoriales.Size = new System.Drawing.Size(183, 295);
+            this.dataGridViewFactoriales.Size = new System.Drawing.Size(181, 295);
             this.dataGridViewFactoriales.TabIndex = 1;
             // 
             // dataGridViewPotencias
@@ -76,7 +75,7 @@
             this.dataGridViewPotencias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewPotencias.Location = new System.Drawing.Point(462, 137);
             this.dataGridViewPotencias.Name = "dataGridViewPotencias";
-            this.dataGridViewPotencias.Size = new System.Drawing.Size(120, 295);
+            this.dataGridViewPotencias.Size = new System.Drawing.Size(118, 295);
             this.dataGridViewPotencias.TabIndex = 2;
             // 
             // dataGridViewTablas
@@ -123,12 +122,12 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Tipo";
             // 
-            // TXT_Prueba
+            // TXT_Hilos
             // 
-            this.TXT_Prueba.Location = new System.Drawing.Point(171, 47);
-            this.TXT_Prueba.Name = "TXT_Prueba";
-            this.TXT_Prueba.Size = new System.Drawing.Size(62, 20);
-            this.TXT_Prueba.TabIndex = 9;
+            this.TXT_Hilos.Location = new System.Drawing.Point(171, 47);
+            this.TXT_Hilos.Name = "TXT_Hilos";
+            this.TXT_Hilos.Size = new System.Drawing.Size(62, 20);
+            this.TXT_Hilos.TabIndex = 9;
             // 
             // label4
             // 
@@ -142,7 +141,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(36, 112);
+            this.label5.Location = new System.Drawing.Point(36, 121);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(82, 13);
             this.label5.TabIndex = 12;
@@ -151,7 +150,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(256, 112);
+            this.label6.Location = new System.Drawing.Point(275, 121);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(84, 13);
             this.label6.TabIndex = 14;
@@ -184,19 +183,19 @@
             this.label9.TabIndex = 15;
             this.label9.Text = "Potencia de ";
             // 
-            // label10
+            // lblNumero
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(704, 112);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(47, 13);
-            this.label10.TabIndex = 18;
-            this.label10.Text = "Número ";
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(702, 121);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(54, 13);
+            this.lblNumero.TabIndex = 18;
+            this.lblNumero.Text = "Multiplicar";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(711, 90);
+            this.label11.Location = new System.Drawing.Point(702, 99);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(49, 13);
             this.label11.TabIndex = 17;
@@ -211,15 +210,6 @@
             this.btn_Salir.Text = "Salir";
             this.btn_Salir.UseVisualStyleBackColor = true;
             this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(297, 472);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 27);
-            this.button1.TabIndex = 20;
-            this.button1.Text = "Prueba";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // TXT_Numeros
             // 
@@ -242,9 +232,8 @@
             this.ClientSize = new System.Drawing.Size(857, 523);
             this.Controls.Add(this.TXT_Tipo);
             this.Controls.Add(this.TXT_Numeros);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_Salir);
-            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblNumero);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label9);
@@ -252,7 +241,7 @@
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.TXT_Prueba);
+            this.Controls.Add(this.TXT_Hilos);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -262,7 +251,9 @@
             this.Controls.Add(this.dataGridViewFactoriales);
             this.Controls.Add(this.dataGridViewNumeros);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Proyecto Hilos";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNumeros)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFactoriales)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPotencias)).EndInit();
@@ -282,17 +273,16 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox TXT_Prueba;
+        private System.Windows.Forms.TextBox TXT_Hilos;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblNumero;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Button btn_Salir;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox TXT_Numeros;
         private System.Windows.Forms.TextBox TXT_Tipo;
     }
---
+++ Proyecto1_Hilos/Proyecto Hilos.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Proyecto1_Hilos
+{
+    public partial class Form1 : Form
+    {
+        // Declaración de variables de hilos y arreglos estáticos para almacenar resultados
+        private Thread hiloFactorial;
+        private Thread hiloPotencias;
+        private Thread hiloMultiplicar;
+        private static long[] factoriales = new long[10];
+        private static int[] potencias = new int[10];
+        private int indexFactorial = 0;
+        private int indexPotencias = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Eventos de carga y redimensionamiento del formulario
+            this.Load += new EventHandler(Form1_Load);
+            this.Resize += new EventHandler(Form1_Resize);
+        }
+
+        // Método ejecutado al cargar el formulario
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Configuración inicial de los DataGridView
+            ConfigureDataGridView(dataGridViewNumeros);
+            ConfigureDataGridView(dataGridViewTablas);
+            ConfigureDataGridView(dataGridViewFactoriales);
+            ConfigureDataGridView(dataGridViewPotencias);
+        }
+
+        // Método ejecutado al redimensionar el formulario
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // Ajuste de la ubicación de los DataGridView al redimensionar
+            AdjustDataGridViewLocation(dataGridViewNumeros);
+            AdjustDataGridViewLocation(dataGridViewTablas);
+            AdjustDataGridViewLocation(dataGridViewFactoriales);
+            AdjustDataGridViewLocation(dataGridViewPotencias);
+        }
+
+        // Ajusta la ubicación horizontal de un DataGridView en relación al formulario
+        private void AdjustDataGridViewLocation(DataGridView dgv)
+        {
+            dgv.Location = new Point(this.ClientSize.Width - dgv.Width - 10, dgv.Location.Y);
+        }
+
+        // Método principal para ejecutar la generación de tablas de multiplicar
+        private void Ejecutar()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Thread.Sleep(1000); // Espera de 1 segundo
+                Invoke((MethodInvoker)delegate
+                {
+                    // Actualización de datos en la interfaz gráfica mediante Invoke
+                    if (i == 1)
+                    {
+                        dataGridViewNumeros.DataSource = CrearTablaNumeros();
+                        ConfigureDataGridView(dataGridViewNumeros);
+                    }
+                    dataGridViewTablas.DataSource = CrearTablaMultiplicar(i);
+                    ConfigureDataGridView(dataGridViewTablas);
+
+                    TXT_Numeros.Text = i.ToString();
+                    TXT_Tipo.Text = DeterminarParidad(i);
+                });
+            }
+        }
+
+        // Método para calcular y almacenar factoriales en un hilo separado
+        private void VectorFactorial()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Thread.Sleep(1000); // Espera de 1 segundo
+                if (i % 2 != 0)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        TXT_Hilos.Text = hiloFactorial.ManagedThreadId.ToString();
+                    }));
+
+                    long factorial = 1;
+                    for (int j = 1; j <= i; j++)
+                    {
+                        factorial *= j;
+                    }
+                    factoriales[indexFactorial++] = factorial;
+                }
+            }
+
+            Invoke((MethodInvoker)delegate
+            {
+                // Actualización de DataGridView con los factoriales calculados
+                dataGridViewFactoriales.DataSource = CrearTablaFactorial();
+                ConfigureDataGridView(dataGridViewFactoriales);
+            });
+        }
+
+        // Método para calcular y almacenar potencias en un hilo separado
+        private void VectorPotencias()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Thread.Sleep(1000); // Espera de 1 segundo
+                if (i % 2 == 0)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        TXT_Hilos.Text = hiloPotencias.ManagedThreadId.ToString();
+                    }));
+
+                    int potencia = i * i;
+                    potencias[indexPotencias++] = potencia;
+                }
+            }
+
+            Invoke((MethodInvoker)delegate
+            {
+                // Actualización de DataGridView con las potencias calculadas
+                dataGridViewPotencias.DataSource = CrearTablaPotencias();
+                ConfigureDataGridView(dataGridViewPotencias);
+            });
+        }
+
+        // Método para crear una tabla de multiplicar específica
+        private DataTable CrearTablaMultiplicar(int n)
+        {
+            var tablaMultiplicar = new DataTable();
+            tablaMultiplicar.Columns.Add("Tabla del " + n);
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = n * i;
+                tablaMultiplicar.Rows.Add($"{n} * {i} = {resultado}");
+            }
+            return tablaMultiplicar;
+        }
+
+        // Método para crear una tabla con números del 1 al 20
+        private DataTable CrearTablaNumeros()
+        {
+            var tablaNumeros = new DataTable();
+            tablaNumeros.Columns.Add("Numero");
+            for (int i = 1; i <= 20; i++)
+            {
+                tablaNumeros.Rows.Add(i);
+            }
+            return tablaNumeros;
+        }
+
+        // Método para crear una tabla con factoriales calculados
+        private DataTable CrearTablaFactorial()
+        {
+            var tablaFactorial = new DataTable();
+            tablaFactorial.Columns.Add("Factorial");
+            int n = 1;
+            foreach (var item in factoriales)
+            {
+                tablaFactorial.Rows.Add($"{n}! = {item}");
+                n += 2;
+            }
+            return tablaFactorial;
+        }
+
+        // Método para crear una tabla con potencias calculadas
+        private DataTable CrearTablaPotencias()
+        {
+            var tablaPotencias = new DataTable();
+            tablaPotencias.Columns.Add("Potencia");
+            int n = 2;
+            foreach (var item in potencias)
+            {
+                tablaPotencias.Rows.Add($"{n} ^ 2 = {item}");
+                n += 2;
+            }
+            return tablaPotencias;
+        }
+
+        // Determina si un número es par o impar
+        private string DeterminarParidad(int i)
+        {
+            return i % 2 == 0 ? "par" : "impar";
+        }
+
+        // Configura el modo de ajuste y alineación de columnas en un DataGridView
+        private void ConfigureDataGridView(DataGridView dgv)
+        {
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            foreach (DataGridViewColumn column in dgv.Columns)
+            {
+                column.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            }
+        }
+
+        // Evento de clic en el botón "Salir" para cerrar la aplicación
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // Evento de clic en el botón "Iniciar" para iniciar los hilos de cálculo
+        private void Btn_Iniciar_Click(object sender, EventArgs e)
+        {
+            hiloFactorial = new Thread(VectorFactorial);
+            hiloPotencias = new Thread(VectorPotencias);
+            hiloMultiplicar = new Thread(Ejecutar);
+
+            hiloFactorial.Start();
+            hiloPotencias.Start();
+            hiloMultiplicar.Start();
+        }
+    }
+}
